eda855c5aa02e94c20c286910cfe2fb30f4e0ab1
hayden <hayden00721@gmail.com>
2024-04-24 21:39:59 +0900

    04/23 reserve/list list, paging 수정

---
+++ src/main/java/com/dev/shop/member/dto/ReservationCriteriaDto.java
@@ -0,0 +1,31 @@
+package com.dev.shop.member.dto;
+
+import com.dev.shop.utils.Pagination;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter @Setter @ToString
+public class ReservationCriteriaDto {
+    private int page;   // 현재 페이지 번호
+    private int recordSize; // 페이지당 출력할 데이터 개수
+    private int pageSize; // 하단에 출력될 페이지 사이즈
+
+    private String keyword; // 검색 키워드
+    private String searchType; // 검색 타입
+
+    private Pagination pagination;
+
+    public ReservationCriteriaDto() {
+        this.page = 1;
+        this.recordSize = 9;
+        this.pageSize = 5;
+    }
+
+    public String[] getTypeArr() {
+        return keyword == null ? new String[]{} : keyword.split("");
+    }
+
+}
